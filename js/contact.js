bb7fec21b64e9494b6e6a3d37d09ddc3249921d5
Mbersun <84075728+Mbersun@users.noreply.github.com>
2021-09-20 22:28:22 +0300

    Responsive Update.

--- js/contact.js
+++ js/contact.js
@@ -1,7 +1,9 @@
-function pop(){
-    var name = document.getElementById("fullname").value;
-    var email = document.getElementById("email").value;
-    var phone = document.getElementById("phone").value;
-    var message = document.getElementById("message").value;
-    alert(` \n Name: ${name} \n Email: ${email} \n Phone: ${phone} \n Message: ${message}`);
+function pop() {
+  var name = document.getElementById("fullname").value;
+  var email = document.getElementById("email").value;
+  var phone = document.getElementById("phone").value;
+  var message = document.getElementById("message").value;
+  alert(
+    ` \n Name: ${name} \n Email: ${email} \n Phone: ${phone} \n Message: ${message}`
+  );
 }
